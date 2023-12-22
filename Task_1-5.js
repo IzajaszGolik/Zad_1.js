e23d37ae38a2e137e30d0a453df26446cf1252c5
IzajaszGolik <izajaszgolik@icloud.com>
2023-12-22 17:08:27 +0100

    first commit

--- Task_1-5.js
+++ Task_1-5.js
@@ -2,3 +2,24 @@ const x = 10;
 const y = 5;
 const amount = x + y;
 console.log(amount);
+
+const firstName = "Izajasz";
+const welcome = `Hello ${firstName}`;
+console.log(welcome);
+
+const length = 13;
+const width = 8;
+const field = (length * width);
+console.log(`${field}` + 'cm');
+
+const quantity = 0;
+const text = "Izajasz";
+console.log(quantity, typeof quantity);
+console.log(text, typeof text);
+
+let colors = ["red", "yellow", "blue"];
+console.log(colors);
+
+const firstName1 = "Izajasz";
+const age = 17;
+console.log(firstName1, age);
