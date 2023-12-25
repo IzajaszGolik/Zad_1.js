202a5ffb3b00655d5afe1f3419724169c3653610
IzajaszGolik <izajaszgolik@icloud.com>
2023-12-25 20:49:47 +0100

    next commit

--- Task_1-5.js
+++ Task_1-5.js
@@ -1,3 +1,5 @@
+// Variables, types JS, AND OR, If else, ternary operator
+
 const x = 10;
 const y = 5;
 const amount = x + y;
@@ -23,3 +25,25 @@ console.log(colors);
 const firstName1 = "Izajasz";
 const age = 17;
 console.log(firstName1, age);
+
+const data = 'some_data';
+console.log(!!data, 'test');
+if(data && !!data) {
+    console.log("Poprawnie załadowano dane");
+}
+else {
+    console.log("Brak danych do wyświetlenia");
+}
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(`Losowana liczba: ${randomNumber}`);
+if (randomNumber <=5) {
+    console.log('Wartość zmiennej jest z przedziału od 1 do 5');
+}
+else {
+    console.log('Wartość zmiennej jest z przedziału od 6 do 10');
+}
+
+const nam = '1';
+const result = 1 == nam ? 'one' : 'two';
+console.log(result);
